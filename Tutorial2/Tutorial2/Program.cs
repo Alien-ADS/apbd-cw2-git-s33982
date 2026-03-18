e97f8aa716a39c2296d6647bedcad711f94ad13f
Szymon Górecki <s33982@pjwstk.edu.pl>
2026-03-18 14:43:33 +0100

    CalculateAverage method added

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -2,10 +2,16 @@
 
 Console.WriteLine("Hello, World!");
 
+static void CalculateAverage(int[] values) {
+    
+}
+
 class Testowy {
     public static void Main(string[] args) {
         Console.WriteLine("Main");
     }
+    
+    
 
     static int multiply(int a, int b) {
         return a * b;
