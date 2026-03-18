95f37ca8e9e055340dca8fdc38a24d8c587df456
Szymon Górecki <s33982@pjwstk.edu.pl>
2026-03-18 15:22:51 +0100

    Changed welcome text

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -11,7 +11,7 @@ static int CalculateMax(int[] values) {
 }
 
 static void Hello() {
-    Console.WriteLine("Hello!");
+    Console.WriteLine("Hallo!");
 }
 
 static int CalculateMin(int[] values) {
